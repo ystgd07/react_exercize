2e5eaa54813c2f52d6687e0f36b50e28f2f215a5
yang <ysotgood@gmail.com>
2022-10-13 04:32:13 +0900

    add_second_useCallback()

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from "react";
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+import DemoOuput from "./components/Demo/DemoOuput";
+function App(props) {
+  const [isValid, setIsValid] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-import './App.css';
-
-function App() {
+  const addOnclickHandler = useCallback(() => {
+    if (allowToggle) {
+      setIsValid((prev) => !prev);
+    }
+  }, [allowToggle]);
+  const allowToggleHandler = () => {
+    setAllowToggle(!allowToggle);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOuput show={isValid}>hi</DemoOuput>
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={addOnclickHandler}>Toggle click</Button>
     </div>
   );
 }
